263b84c46f1aa58091eb6a82f58acaeb4b5ef574
Rainey <23113728+raineydavid@users.noreply.github.com>
2019-08-06 10:38:11 +0100

    Update playlist.js

--- src/templates/playlist.js
+++ src/templates/playlist.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import ListItem from '../components/ListItem'
 
 export const PlaylistTemplate = ({
   content,
